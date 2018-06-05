b5f24341c025fe6773346b2eda0e73f98b5cadb8
Marc-Antoine <maheux.marcantoine@gmail.com>
2018-06-04 21:15:39 -0400

    Add beer tags. Add beer ordering. Add beer filtering

--- resources/sql/operations_beer.sql
+++ resources/sql/operations_beer.sql
@@ -1,8 +1,207 @@
--- name: get-beer
-SELECT id_beer AS id, name, description, ibu, alcohol_percent, image_path FROM beer WHERE id_beer = :id;
--- name: get-beers
-SELECT id_beer AS id, name, description, ibu, alcohol_percent, image_path FROM beer LIMIT :first OFFSET :skip;
 -- name: get-beer-breweries
 SELECT brewery.id_brewery AS id, name, description, image_path FROM brewery
     INNER JOIN beer_brewery ON beer_brewery.id_brewery = brewery.id_brewery
     WHERE id_beer = :id;
+
+-- name: get-beer-tags
+SELECT tag.id_tag as id, name FROM tag
+    INNER JOIN beer_tag ON beer_tag.id_tag = tag.id_tag
+    WHERE id_beer = :id;
+
+
+-- name: get-beer
+SELECT id_beer AS id, name, description, ibu, alcohol_percent, image_path FROM beer WHERE id_beer = :id;
+
+
+-- name: get-beers-ordered-by-name-asc
+SELECT id_beer AS id, name, description, ibu, alcohol_percent, image_path FROM beer
+    ORDER BY name ASC
+    LIMIT :first OFFSET :skip;
+
+-- name: get-beers-ordered-by-name-desc
+SELECT id_beer AS id, name, description, ibu, alcohol_percent, image_path FROM beer
+    ORDER BY name DESC
+    LIMIT :first OFFSET :skip;
+
+
+-- name: get-beers-filtered-by-breweries-ordered-by-name-asc
+SELECT beer.id_beer AS id, beer.name, description, ibu, alcohol_percent, image_path FROM beer
+    INNER JOIN beer_brewery ON beer_brewery.id_beer = beer.id_beer
+    WHERE id_brewery IN (:breweries)
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY name ASC
+    LIMIT :first OFFSET :skip;
+
+-- name: get-beers-filtered-by-breweries-ordered-by-name-desc
+SELECT beer.id_beer AS id, beer.name, description, ibu, alcohol_percent, image_path FROM beer
+    INNER JOIN beer_brewery ON beer_brewery.id_beer = beer.id_beer
+    WHERE id_brewery IN (:breweries)
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY name DESC
+    LIMIT :first OFFSET :skip;
+
+
+-- name: get-beers-filtered-by-tags-ordered-by-name-asc
+SELECT beer.id_beer AS id, beer.name, description, ibu, alcohol_percent, image_path FROM beer
+    INNER JOIN beer_tag ON beer_tag.id_beer = beer.id_beer
+    WHERE id_tag IN (:tags)
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY name ASC
+    LIMIT :first OFFSET :skip;
+
+-- name: get-beers-filtered-by-tags-ordered-by-name-desc
+SELECT beer.id_beer AS id, beer.name, description, ibu, alcohol_percent, image_path FROM beer
+    INNER JOIN beer_tag ON beer_tag.id_beer = beer.id_beer
+    WHERE id_tag IN (:tags)
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY name DESC
+    LIMIT :first OFFSET :skip;
+
+
+-- name: get-beers-filtered-by-breweries-tags-ordered-by-name-asc
+SELECT beer.id_beer AS id, beer.name, description, ibu, alcohol_percent, image_path FROM beer
+    INNER JOIN beer_brewery ON beer_brewery.id_beer = beer.id_beer
+    INNER JOIN beer_tag ON beer_tag.id_beer = beer.id_beer
+    WHERE id_brewery IN (:breweries) AND id_tag IN (:tags)
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY name ASC
+    LIMIT :first OFFSET :skip;
+
+-- name: get-beers-filtered-by-breweries-tags-ordered-by-name-desc
+SELECT beer.id_beer AS id, beer.name, description, ibu, alcohol_percent, image_path FROM beer
+    INNER JOIN beer_brewery ON beer_brewery.id_beer = beer.id_beer
+    INNER JOIN beer_tag ON beer_tag.id_beer = beer.id_beer
+    WHERE id_brewery IN (:breweries) AND id_tag IN (:tags)
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY name DESC
+    LIMIT :first OFFSET :skip;
+
+
+-- name: get-beers-ordered-by-rating-asc
+WITH all_beer_ratings AS (
+    SELECT id_beer, 0.0 as rating FROM beer
+    UNION ALL
+    SELECT id_beer, rating FROM beer_review
+    UNION ALL
+    SELECT id_beer, rating FROM beer_user_rating
+)
+SELECT beer.id_beer AS id, name, description, ibu, alcohol_percent, image_path FROM beer
+    LEFT JOIN all_beer_ratings ON all_beer_ratings.id_beer = beer.id_beer
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY avg(rating) ASC, name ASC
+    LIMIT :first OFFSET :skip;
+
+-- name: get-beers-ordered-by-rating-desc
+WITH all_beer_ratings AS (
+    SELECT id_beer, 0.0 as rating FROM beer
+    UNION ALL
+    SELECT id_beer, rating FROM beer_review
+    UNION ALL
+    SELECT id_beer, rating FROM beer_user_rating
+)
+SELECT beer.id_beer AS id, name, description, ibu, alcohol_percent, image_path FROM beer
+    LEFT JOIN all_beer_ratings ON all_beer_ratings.id_beer = beer.id_beer
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY avg(rating) DESC, name ASC
+    LIMIT :first OFFSET :skip;
+
+
+-- name: get-beers-filtered-by-breweries-ordered-by-rating-asc
+WITH all_beer_ratings AS (
+    SELECT id_beer, 0.0 as rating FROM beer
+    UNION ALL
+    SELECT id_beer, rating FROM beer_review
+    UNION ALL
+    SELECT id_beer, rating FROM beer_user_rating
+)
+SELECT beer.id_beer AS id, name, description, ibu, alcohol_percent, image_path FROM beer
+    LEFT JOIN all_beer_ratings ON all_beer_ratings.id_beer = beer.id_beer
+    INNER JOIN beer_brewery ON beer_brewery.id_beer = beer.id_beer
+    WHERE id_brewery IN (:breweries)
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY avg(rating) ASC, name ASC
+    LIMIT :first OFFSET :skip;
+
+-- name: get-beers-filtered-by-breweries-ordered-by-rating-desc
+WITH all_beer_ratings AS (
+    SELECT id_beer, 0.0 as rating FROM beer
+    UNION ALL
+    SELECT id_beer, rating FROM beer_review
+    UNION ALL
+    SELECT id_beer, rating FROM beer_user_rating
+)
+SELECT beer.id_beer AS id, name, description, ibu, alcohol_percent, image_path FROM beer
+    LEFT JOIN all_beer_ratings ON all_beer_ratings.id_beer = beer.id_beer
+    INNER JOIN beer_brewery ON beer_brewery.id_beer = beer.id_beer
+    WHERE id_brewery IN (:breweries)
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY avg(rating) DESC, name ASC
+    LIMIT :first OFFSET :skip;
+
+
+-- name: get-beers-filtered-by-tags-ordered-by-rating-asc
+WITH all_beer_ratings AS (
+    SELECT id_beer, 0.0 as rating FROM beer
+    UNION ALL
+    SELECT id_beer, rating FROM beer_review
+    UNION ALL
+    SELECT id_beer, rating FROM beer_user_rating
+)
+SELECT beer.id_beer AS id, name, description, ibu, alcohol_percent, image_path FROM beer
+    LEFT JOIN all_beer_ratings ON all_beer_ratings.id_beer = beer.id_beer
+    INNER JOIN beer_tag ON beer_tag.id_beer = beer.id_beer
+    WHERE id_tag IN (:tags)
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY avg(rating) ASC, name ASC
+    LIMIT :first OFFSET :skip;
+
+-- name: get-beers-filtered-by-tags-ordered-by-rating-desc
+WITH all_beer_ratings AS (
+    SELECT id_beer, 0.0 as rating FROM beer
+    UNION ALL
+    SELECT id_beer, rating FROM beer_review
+    UNION ALL
+    SELECT id_beer, rating FROM beer_user_rating
+)
+SELECT beer.id_beer AS id, name, description, ibu, alcohol_percent, image_path FROM beer
+    LEFT JOIN all_beer_ratings ON all_beer_ratings.id_beer = beer.id_beer
+    INNER JOIN beer_tag ON beer_tag.id_beer = beer.id_beer
+    WHERE id_tag IN (:tags)
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY avg(rating) DESC, name ASC
+    LIMIT :first OFFSET :skip;
+
+
+-- name: get-beers-filtered-by-breweries-tags-ordered-by-rating-asc
+WITH all_beer_ratings AS (
+    SELECT id_beer, 0.0 as rating FROM beer
+    UNION ALL
+    SELECT id_beer, rating FROM beer_review
+    UNION ALL
+    SELECT id_beer, rating FROM beer_user_rating
+)
+SELECT beer.id_beer AS id, name, description, ibu, alcohol_percent, image_path FROM beer
+    LEFT JOIN all_beer_ratings ON all_beer_ratings.id_beer = beer.id_beer
+    INNER JOIN beer_brewery ON beer_brewery.id_beer = beer.id_beer
+    INNER JOIN beer_tag ON beer_tag.id_beer = beer.id_beer
+    WHERE id_brewery IN (:breweries) AND id_tag IN (:tags)
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY avg(rating) ASC, name ASC
+    LIMIT :first OFFSET :skip;
+
+-- name: get-beers-filtered-by-breweries-tags-ordered-by-rating-desc
+WITH all_beer_ratings AS (
+    SELECT id_beer, 0.0 as rating FROM beer
+    UNION ALL
+    SELECT id_beer, rating FROM beer_review
+    UNION ALL
+    SELECT id_beer, rating FROM beer_user_rating
+)
+SELECT beer.id_beer AS id, name, description, ibu, alcohol_percent, image_path FROM beer
+    LEFT JOIN all_beer_ratings ON all_beer_ratings.id_beer = beer.id_beer
+    INNER JOIN beer_brewery ON beer_brewery.id_beer = beer.id_beer
+    INNER JOIN beer_tag ON beer_tag.id_beer = beer.id_beer
+    WHERE id_brewery IN (:breweries) AND id_tag IN (:tags)
+    GROUP BY beer.id_beer, beer.name, description, ibu, alcohol_percent, image_path
+    ORDER BY avg(rating) DESC, name ASC
+    LIMIT :first OFFSET :skip;
--- src/beerpressure/db/beer.clj
+++ src/beerpressure/db/beer.clj
@@ -1,21 +1,57 @@
 (ns beerpressure.db.beer
   (:require [environ.core :refer [env]]
             [yesql.core :refer [defqueries]]
-            [beerpressure.db.common :refer :all]))
+            [beerpressure.db.common :refer :all]
+            [beerpressure.db.brewery :refer [fill-brewery-tags]]))
 
 (defqueries "sql/operations_beer.sql"
             {:connection db-spec})
 
 (defn fill-beer-breweries
   [beer]
-  (assoc beer :breweries (convert-naming-convention (get-beer-breweries {:id (get beer :id)}))))
+  (assoc beer :breweries (convert-naming-convention
+                           (map #(fill-brewery-tags %) (get-beer-breweries {:id (get beer :id)})))))
+
+(defn fill-beer-tags
+  [beer]
+  (assoc beer :tags (get-beer-tags {:id (get beer :id)})))
 
 (defn resolve-beer
   [context args _value]
   (let [beer (first (convert-naming-convention (check-error (get-beer args))))]
-    (fill-beer-breweries beer)))
+    (fill-beer-tags (fill-beer-breweries beer))))
 
 (defn resolve-beers
   [context args _value]
-  (let [beers (convert-naming-convention (check-error (get-beers args)))]
-    (map #(fill-beer-breweries %) beers)))
+  (let [beers (check-error (case (get args :orderBy)
+                :NAME (case (get args :breweries)
+                        [] (case (get args :tags)
+                             [] (case (get args :orderType)
+                                  :ASC (get-beers-ordered-by-name-asc args)
+                                  :DESC (get-beers-ordered-by-name-desc args))
+                             (case (get args :orderType)
+                               :ASC (get-beers-filtered-by-tags-ordered-by-name-asc args)
+                               :DESC (get-beers-filtered-by-tags-ordered-by-name-desc args)))
+                        (case (get args :tags)
+                          [] (case (get args :orderType)
+                               :ASC (get-beers-filtered-by-breweries-ordered-by-name-asc args)
+                               :DESC (get-beers-filtered-by-breweries-ordered-by-name-desc args))
+                          (case (get args :orderType)
+                            :ASC (get-beers-filtered-by-breweries-tags-ordered-by-name-asc args)
+                            :DESC (get-beers-filtered-by-breweries-tags-ordered-by-name-desc args))))
+                :RATING (case (get args :breweries)
+                          [] (case (get args :tags)
+                               [] (case (get args :orderType)
+                                    :ASC (get-beers-ordered-by-rating-asc args)
+                                    :DESC (get-beers-ordered-by-rating-desc args))
+                               (case (get args :orderType)
+                                 :ASC (get-beers-filtered-by-tags-ordered-by-rating-asc args)
+                                 :DESC (get-beers-filtered-by-tags-ordered-by-rating-desc args)))
+                          (case (get args :tags)
+                            [] (case (get args :orderType)
+                                 :ASC (get-beers-filtered-by-breweries-ordered-by-rating-asc args)
+                                 :DESC (get-beers-filtered-by-breweries-ordered-by-rating-desc args))
+                            (case (get args :orderType)
+                              :ASC (get-beers-filtered-by-breweries-tags-ordered-by-rating-asc args)
+                              :DESC (get-beers-filtered-by-breweries-tags-ordered-by-rating-desc args))))))]
+    (convert-naming-convention (map #(fill-beer-tags (fill-beer-breweries %)) beers))))
