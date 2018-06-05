f4076e980e21af83ddaee2a232a84f15bdf2fcdd
Marc-Antoine <maheux.marcantoine@gmail.com>
2018-06-04 22:23:37 -0400

    Remove duplicate SQL code (VIEW). Add rating fields (beer, brewery).

--- resources/sql/operations_brewery.sql
+++ resources/sql/operations_brewery.sql
@@ -1,99 +1,64 @@
 -- name: get-brewery-tags
-SELECT tag.id_tag as id, name FROM tag
+SELECT tag.id_tag AS id, name FROM tag
     INNER JOIN tag_brewery ON tag_brewery.id_tag = tag.id_tag
     WHERE id_brewery = :id;
 
 
 -- name: get-brewery
-SELECT id_brewery AS id, name, description, image_path FROM brewery WHERE id_brewery = :id;
+SELECT id_brewery AS id, name, description, image_path, rating FROM brewery_with_rating WHERE id_brewery = :id;
 
 
 -- name: get-breweries-ordered-by-name-asc
-SELECT id_brewery AS id, name, description, image_path FROM brewery
+SELECT id_brewery AS id, name, description, image_path, rating FROM brewery_with_rating
     ORDER BY name ASC
     LIMIT :first OFFSET :skip;
 
 -- name: get-breweries-ordered-by-name-desc
-SELECT id_brewery AS id, name, description, image_path FROM brewery
+SELECT id_brewery AS id, name, description, image_path, rating FROM brewery_with_rating
     ORDER BY name DESC
     LIMIT :first OFFSET :skip;
 
 
 -- name: get-breweries-filtered-by-tags-ordered-by-name-asc
-SELECT brewery.id_brewery AS id, name, description, image_path FROM brewery
+SELECT brewery.id_brewery AS id, name, description, image_path, rating FROM brewery_with_rating AS brewery
     INNER JOIN tag_brewery ON tag_brewery.id_brewery = brewery.id_brewery
     WHERE id_tag IN (:tags)
-    GROUP BY brewery.id_brewery, name, description, image_path
+    GROUP BY brewery.id_brewery, name, description, image_path, rating
     ORDER BY name ASC
     LIMIT :first OFFSET :skip;
 
 -- name: get-breweries-filtered-by-tags-ordered-by-name-desc
-SELECT brewery.id_brewery AS id, name, description, image_path FROM brewery
+SELECT brewery.id_brewery AS id, name, description, image_path, rating FROM brewery_with_rating AS brewery
     INNER JOIN tag_brewery ON tag_brewery.id_brewery = brewery.id_brewery
     WHERE id_tag IN (:tags)
-    GROUP BY brewery.id_brewery, name, description, image_path
+    GROUP BY brewery.id_brewery, name, description, image_path, rating
     ORDER BY name DESC
     LIMIT :first OFFSET :skip;
 
 
 -- name: get-breweries-ordered-by-rating-asc
-WITH all_brewery_ratings AS (
-    SELECT id_brewery, 0.0 as rating FROM brewery_review
-    UNION ALL
-    SELECT id_brewery, rating FROM brewery_review
-    UNION ALL
-    SELECT id_brewery, rating FROM brewery_user_rating
-)
-SELECT brewery.id_brewery AS id, name, description, image_path FROM brewery
-    LEFT JOIN all_brewery_ratings ON all_brewery_ratings.id_brewery = brewery.id_brewery
-    GROUP BY brewery.id_brewery, name, description, image_path
-    ORDER BY avg(rating) ASC, name ASC
+SELECT id_brewery AS id, name, description, image_path, rating FROM brewery_with_rating
+    ORDER BY rating ASC, name ASC
     LIMIT :first OFFSET :skip;
 
 -- name: get-breweries-ordered-by-rating-desc
-WITH all_brewery_ratings AS (
-    SELECT id_brewery, 0.0 as rating FROM brewery_review
-    UNION ALL
-    SELECT id_brewery, rating FROM brewery_review
-    UNION ALL
-    SELECT id_brewery, rating FROM brewery_user_rating
-)
-SELECT brewery.id_brewery AS id, name, description, image_path FROM brewery
-    LEFT JOIN all_brewery_ratings ON all_brewery_ratings.id_brewery = brewery.id_brewery
-    GROUP BY brewery.id_brewery, name, description, image_path
-    ORDER BY avg(rating) DESC, name ASC
+SELECT id_brewery AS id, name, description, image_path, rating FROM brewery_with_rating
+    ORDER BY rating DESC, name ASC
     LIMIT :first OFFSET :skip;
 
 
 -- name: get-breweries-filtered-by-tags-ordered-by-rating-asc
-WITH all_brewery_ratings AS (
-    SELECT id_brewery, 0.0 as rating FROM brewery_review
-    UNION ALL
-    SELECT id_brewery, rating FROM brewery_review
-    UNION ALL
-    SELECT id_brewery, rating FROM brewery_user_rating
-)
-SELECT brewery.id_brewery AS id, name, description, image_path FROM brewery
+SELECT brewery.id_brewery AS id, name, description, image_path, rating FROM brewery_with_rating AS brewery
     INNER JOIN tag_brewery ON tag_brewery.id_brewery = brewery.id_brewery
-    LEFT JOIN all_brewery_ratings ON all_brewery_ratings.id_brewery = brewery.id_brewery
     WHERE id_tag IN (:tags)
-    GROUP BY brewery.id_brewery, name, description, image_path
-    ORDER BY avg(rating) ASC, name ASC
+    GROUP BY brewery.id_brewery, name, description, image_path, rating
+    ORDER BY rating ASC, name ASC
     LIMIT :first OFFSET :skip;
 
 -- name: get-breweries-filtered-by-tags-ordered-by-rating-desc
-WITH all_brewery_ratings AS (
-    SELECT id_brewery, 0.0 as rating FROM brewery_review
-    UNION ALL
-    SELECT id_brewery, rating FROM brewery_review
-    UNION ALL
-    SELECT id_brewery, rating FROM brewery_user_rating
-)
-SELECT brewery.id_brewery AS id, name, description, image_path FROM brewery
+SELECT brewery.id_brewery AS id, name, description, image_path, rating FROM brewery_with_rating AS brewery
     INNER JOIN tag_brewery ON tag_brewery.id_brewery = brewery.id_brewery
-    LEFT JOIN all_brewery_ratings ON all_brewery_ratings.id_brewery = brewery.id_brewery
     WHERE id_tag IN (:tags)
-    GROUP BY brewery.id_brewery, name, description, image_path
-    ORDER BY avg(rating) DESC, name ASC
+    GROUP BY brewery.id_brewery, name, description, image_path, rating
+    ORDER BY rating DESC, name ASC
     LIMIT :first OFFSET :skip;
-
