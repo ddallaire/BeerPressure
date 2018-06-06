4531b64bb70152264842916a546bf1d66fb98cf8
Etienne Pelletier <etipelletier@hotmail.com>
2018-06-06 14:27:58 -0400

    Adjusted time stamps to fix the tests

--- resources/sql/test-data.sql
+++ resources/sql/test-data.sql
@@ -16,7 +16,7 @@ DELETE FROM brewery;
 DELETE FROM user_follow;
 DELETE FROM "user";
 
-INSERT INTO "user"(cip, name, surname) VALUES 
+INSERT INTO "user"(cip, name, surname) VALUES
     ('alig2503', 'Gaétan', 'Allano'),
     ('dald2202', 'David', 'Dallaire'),
     ('lara2318', 'Andy', 'Larochelle'),
@@ -57,13 +57,13 @@ INSERT INTO beer_brewery(id_brewery, id_beer) VALUES
     (5, 5),
     (6, 5);
 
-INSERT INTO tag (id_tag, name) VALUES 
+INSERT INTO tag (id_tag, name) VALUES
     (1, 'Industrielle'),
     (2, 'Artisanale'),
     (3, 'Bon choix'),
     (4, 'Mauvais choix');
 
-INSERT INTO tag_brewery(id_brewery, id_tag) VALUES 
+INSERT INTO tag_brewery(id_brewery, id_tag) VALUES
     (1, 1),
     (2, 1),
     (3, 2),
@@ -84,7 +84,7 @@ INSERT INTO beer_tag(id_beer, id_tag) VALUES
     (5, 2),
     (5, 4);
 
-INSERT INTO beer_user_drank(cip, id_beer) VALUES 
+INSERT INTO beer_user_drank(cip, id_beer) VALUES
     ('alig2503', 3),
     ('alig2503', 4),
     ('dald2202', 3),
@@ -126,22 +126,22 @@ INSERT INTO brewery_user_rating(cip, id_brewery, rating) VALUES
     ('royj1933', 3, 1.0);
 
 INSERT INTO beer_review(id_beer_review, cip, id_beer, title, content, image_path, rating, time) VALUES
-    (1, 'alig2503', 1, 'Une mauvaise fierté canadienne', 'Le titre était mon contenu... Il n''a vraiment rien à dire.', NULL, 1.0, now() - INTERVAL '20 minute'),
-    (2, 'dald2202', 3, 'Beaucoup d''alcool!', 'C''est parfait pour être saoul!!!', 'http://benpourquoipas.com/wp-content/uploads/2012/10/bebe-saoul-marcher-sobre.jpg', 2.5, now() - INTERVAL '19 minute'),
-    (3, 'lara2318', 4, 'Que c''est bon!!', 'C''est pas pire pour une soirée entre boux!!', NULL, 4.5, now() - INTERVAL '18 minute'),
-    (4, 'mahm1904', 5, 'Je n''aime pas la bière, mais ça se boit!!', 'Ça se vend au Siboire... Je suis con c''est écrit dans la description.', NULL, 1.5, now() - INTERVAL '17 minute'),
-    (5, 'parp2009', 1, 'Un autre truc pas bon!', 'CE N''EST PAS BON...', NULL, 2.0, now() - INTERVAL '16 minute'),
-    (6, 'pele1704', 4, 'Tu as raison Andy!!', 'Damn... c''est un review, pas un commentaire...', 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjn1Pr_wLPbAhXKtlkKHX7EBJAQjRx6BAgBEAU&url=http%3A%2F%2Fknowyourmeme.com%2Fphotos%2F56807-fail-epic-fail&psig=AOvVaw3HfgoRpGtV9HRR5-0cLFJ0&ust=1527978321220516', 4.0, now() - INTERVAL '15 minute'),
-    (7, 'royj1933', 4, 'Époustouflant!!!', 'Je n''ai pas d''autre', NULL, 5.0, now() - INTERVAL '14 minute');
+    (1, 'alig2503', 1, 'Une mauvaise fierté canadienne', 'Le titre était mon contenu... Il n''a vraiment rien à dire.', NULL, 1.0, TIMESTAMP '2018-06-10 10:00:00-4'),
+    (2, 'dald2202', 3, 'Beaucoup d''alcool!', 'C''est parfait pour être saoul!!!', 'http://benpourquoipas.com/wp-content/uploads/2012/10/bebe-saoul-marcher-sobre.jpg', 2.5, TIMESTAMP '2018-06-10 10:00:01-4'),
+    (3, 'lara2318', 4, 'Que c''est bon!!', 'C''est pas pire pour une soirée entre boux!!', NULL, 4.5, TIMESTAMP '2018-06-10 10:00:02-4'),
+    (4, 'mahm1904', 5, 'Je n''aime pas la bière, mais ça se boit!!', 'Ça se vend au Siboire... Je suis con c''est écrit dans la description.', NULL, 1.5, TIMESTAMP '2018-06-10 10:00:03-4'),
+    (5, 'parp2009', 1, 'Un autre truc pas bon!', 'CE N''EST PAS BON...', NULL, 2.0, TIMESTAMP '2018-06-10 10:00:04-4'),
+    (6, 'pele1704', 4, 'Tu as raison Andy!!', 'Damn... c''est un review, pas un commentaire...', 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjn1Pr_wLPbAhXKtlkKHX7EBJAQjRx6BAgBEAU&url=http%3A%2F%2Fknowyourmeme.com%2Fphotos%2F56807-fail-epic-fail&psig=AOvVaw3HfgoRpGtV9HRR5-0cLFJ0&ust=1527978321220516', 4.0, TIMESTAMP '2018-06-10 10:00:05-4'),
+    (7, 'royj1933', 4, 'Époustouflant!!!', 'Je n''ai pas d''autre', NULL, 5.0, TIMESTAMP '2018-06-10 10:00:06-4');
 
 INSERT INTO beer_review_comment(id_beer_review_comment, id_beer_review, cip, content, time) VALUES
-    (1, 1, 'pele1704', 'Je suis d''accord avec toi!', now() - INTERVAL '10 minute'),
-    (2, 2, 'parp2009', 'J''ai tellement été saoul un soir en buvant ça!!!', now() - INTERVAL '9 minute'),
-    (3, 3, 'mahm1904', 'Peut-être que j''aimerais ça!', now() - INTERVAL '8 minute'),
-    (4, 4, 'lara2318', 'C''est de la marde cette bière... Tu dis n''importe quoi...', now() - INTERVAL '7 minute'),
-    (5, 5, 'dald2202', 'Tu as bien raison!!', now() - INTERVAL '6 minute'),
-    (6, 3, 'alig2503', 'C''est vraiment bon ça!!!', now() - INTERVAL '5 minute'),
-    (7, 3, 'royj1933', 'C''est nice les soirée entre boux! Je fais de la crème brulée', now() - INTERVAL '5 minute');
+    (1, 1, 'pele1704', 'Je suis d''accord avec toi!', TIMESTAMP '2018-06-10 10:00:10-4'),
+    (2, 2, 'parp2009', 'J''ai tellement été saoul un soir en buvant ça!!!', TIMESTAMP '2018-06-10 10:00:11-4'),
+    (3, 3, 'mahm1904', 'Peut-être que j''aimerais ça!', TIMESTAMP '2018-06-10 10:00:12-4'),
+    (4, 4, 'lara2318', 'C''est de la marde cette bière... Tu dis n''importe quoi...', TIMESTAMP '2018-06-10 10:00:13-4'),
+    (5, 5, 'dald2202', 'Tu as bien raison!!', TIMESTAMP '2018-06-10 10:00:14-4'),
+    (6, 3, 'alig2503', 'C''est vraiment bon ça!!!', TIMESTAMP '2018-06-10 10:00:15-4'),
+    (7, 3, 'royj1933', 'C''est nice les soirée entre boux! Je fais de la crème brulée', TIMESTAMP '2018-06-10 10:00:16-4');
 
 INSERT INTO beer_review_user_thumbsup(id_beer_review, cip) VALUES
     (1, 'dald2202'),
@@ -155,22 +155,22 @@ INSERT INTO beer_review_user_thumbsup(id_beer_review, cip) VALUES
     (2, 'royj1933');
 
 INSERT INTO brewery_review(id_brewery_review, cip, id_brewery, title, content, image_path, rating, time) VALUES
-    (1, 'alig2503', 1, 'Une mauvaise brasserie montréalaise...', 'Sans autre commentaire...', NULL, 1.5, now() - INTERVAL '20 minute'),
-    (2, 'dald2202', 2, 'Le brasseur de la 50...', 'Ce n''est pas grand exploit...', 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiylLDdxrPbAhXJjVkKHdQ0AfUQjRx6BAgBEAU&url=http%3A%2F%2Fwww.labatt.com%2Fbrands%2Fnationalbrands.php%3Flanguage%3Dfr&psig=AOvVaw3AY0gWiexTaG1xbjUkwtVH&ust=1527979870192306', 1.5, now() - INTERVAL '19 minute'),
-    (3, 'lara2318', 3, 'Ordinaire...', 'Toutes leurs bières ont le même goût...', NULL, 0.1, now() - INTERVAL '18 minute'),
-    (4, 'mahm1904', 6, 'Encouragement!!!', 'Vive génie sherbrooke!!!', NULL, 4.0, now() - INTERVAL '17 minute'),
-    (5, 'parp2009', 5, 'Bonne bouffe!!', 'Le Mac&Cheese est super.', NULL, 5.0, now() - INTERVAL '16 minute'),
-    (6, 'pele1704', 4, 'Bravo!', 'Ils font de très bonnes bières!!', NULL, 4.5, now() - INTERVAL '15 minute'),
-    (7, 'royj1933', 5, 'Super bouffe!!!', 'Bravo au chef!', NULL, 4.0, now() - INTERVAL '14 minute');
-    
+    (1, 'alig2503', 1, 'Une mauvaise brasserie montréalaise...', 'Sans autre commentaire...', NULL, 1.5, TIMESTAMP '2018-06-10 10:00:00-4'),
+    (2, 'dald2202', 2, 'Le brasseur de la 50...', 'Ce n''est pas grand exploit...', 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiylLDdxrPbAhXJjVkKHdQ0AfUQjRx6BAgBEAU&url=http%3A%2F%2Fwww.labatt.com%2Fbrands%2Fnationalbrands.php%3Flanguage%3Dfr&psig=AOvVaw3AY0gWiexTaG1xbjUkwtVH&ust=1527979870192306', 1.5, TIMESTAMP '2018-06-10 10:00:01-4'),
+    (3, 'lara2318', 3, 'Ordinaire...', 'Toutes leurs bières ont le même goût...', NULL, 0.1, TIMESTAMP '2018-06-10 10:00:02-4'),
+    (4, 'mahm1904', 6, 'Encouragement!!!', 'Vive génie sherbrooke!!!', NULL, 4.0, TIMESTAMP '2018-06-10 10:00:03-4'),
+    (5, 'parp2009', 5, 'Bonne bouffe!!', 'Le Mac&Cheese est super.', NULL, 5.0, TIMESTAMP '2018-06-10 10:00:04-4'),
+    (6, 'pele1704', 4, 'Bravo!', 'Ils font de très bonnes bières!!', NULL, 4.5, TIMESTAMP '2018-06-10 10:00:05-4'),
+    (7, 'royj1933', 5, 'Super bouffe!!!', 'Bravo au chef!', NULL, 4.0, TIMESTAMP '2018-06-10 10:00:06-4');
+
 INSERT INTO brewery_review_comment(id_brewery_review_comment, id_brewery_review, cip, content, time) VALUES
-    (1, 2, 'alig2503', 'Comment oses-tu dire ceci?', now() - INTERVAL '10 minute'),
-    (2, 3, 'dald2202', 'Je suis bien d''accord!', now() - INTERVAL '9 minute'),
-    (3, 4, 'lara2318', 'Tu ne connais rien à la bière...', now() - INTERVAL '8 minute'),
-    (4, 5, 'mahm1904', 'Party!!!', now() - INTERVAL '7 minute'),
-    (5, 5, 'parp2009', 'PARTY!!! PARTY!!! PARTY!!! PARTY!!!',  now() - INTERVAL '6 minute'), 
-    (6, 7, 'pele1704', 'On y va quand?', now() - INTERVAL '5 minute'),
-    (7, 1, 'royj1933', 'Parce que c''est vrai...', now() - INTERVAL '4 minute');
+    (1, 2, 'alig2503', 'Comment oses-tu dire ceci?', TIMESTAMP '2018-06-10 10:00:10-4'),
+    (2, 3, 'dald2202', 'Je suis bien d''accord!', TIMESTAMP '2018-06-10 10:00:11-4'),
+    (3, 4, 'lara2318', 'Tu ne connais rien à la bière...', TIMESTAMP '2018-06-10 10:00:12-4'),
+    (4, 5, 'mahm1904', 'Party!!!', TIMESTAMP '2018-06-10 10:00:13-4'),
+    (5, 5, 'parp2009', 'PARTY!!! PARTY!!! PARTY!!! PARTY!!!',  TIMESTAMP '2018-06-10 10:00:14-4'),
+    (6, 7, 'pele1704', 'On y va quand?', TIMESTAMP '2018-06-10 10:00:15-4'),
+    (7, 1, 'royj1933', 'Parce que c''est vrai...', TIMESTAMP '2018-06-10 10:00:16-4');
 
 INSERT INTO brewery_review_user_thumbsup(id_brewery_review, cip) VALUES
     (1, 'dald2202'),
@@ -182,4 +182,4 @@ INSERT INTO brewery_review_user_thumbsup(id_brewery_review, cip) VALUES
     (6, 'alig2503'),
     (7, 'parp2009'),
     (2, 'royj1933');
-    
+
