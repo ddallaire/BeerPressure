b4875f5808fd49c9f23c20896248401f75560a26
Etienne Pelletier <etipelletier@hotmail.com>
2018-06-13 19:20:12 -0400

    Added missing beer query

--- resources/sql/operations_brewery.sql
+++ resources/sql/operations_brewery.sql
@@ -1,2 +1,7 @@
+-- name: get-brewery-tags
+SELECT tag.id_tag AS id, name FROM tag
+    INNER JOIN tag_brewery ON tag_brewery.id_tag = tag.id_tag
+    WHERE id_brewery = :id;
+
 -- name: get-brewery
 SELECT id_brewery AS id, name, description, image_path, rating FROM brewery_with_rating WHERE id_brewery = :id;
