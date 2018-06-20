22039848199de1ef62ba75bce3621dcbd39fc01d
Marc-Antoine <maheux.marcantoine@gmail.com>
2018-06-20 11:34:13 -0400

    Replace cip by user in brewery review comment object

--- resources/sql/comment/operations_brewery_comment.sql
+++ resources/sql/comment/operations_brewery_comment.sql
@@ -1,3 +1,4 @@
 -- name: get-brewery-review-comment
-SELECT id_brewery_review_comment, cip, id_brewery_review, content, time FROM brewery_review_comment
-WHERE id_brewery_review_comment = :idBreweryReviewComment;
+SELECT id_brewery_review_comment, "user".cip, name, surname, id_brewery_review, content, time FROM brewery_review_comment
+    INNER JOIN "user" ON "user".cip = brewery_review_comment.cip
+    WHERE id_brewery_review_comment = :idBreweryReviewComment;
