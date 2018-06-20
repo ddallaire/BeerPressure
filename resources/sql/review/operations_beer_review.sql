46e6aed1a8905853a58765439974d28570f8754e
mamaheux <35638081+mamaheux@users.noreply.github.com>
2018-06-19 23:46:20 -0400

    Feature/review filters (#10)
    * Add ordering, filtering and thumbs-ups to beer reviews.
    
    * Add ordering, filtering and thumbsup to brewery review
    
    * Add brewery review filtering and ordering tests
    
    * Add beer review filtering and ordering tests

--- resources/sql/review/operations_beer_review.sql
+++ resources/sql/review/operations_beer_review.sql
@@ -1,6 +1,8 @@
 -- name: get-beer-review
 SELECT id_beer_review, cip, id_beer, title, content, image_path, rating, time FROM Beer_Review WHERE id_beer_review = :idBeerReview;
 
--- name: get-beer-reviews
-SELECT id_beer_review, cip, id_beer, title, content, image_path, rating, time FROM Beer_Review
-LIMIT :first OFFSET :skip;
+-- name: get-beer-review-thumbsups
+SELECT "user".cip, name, surname FROM beer_review_user_thumbsup
+    INNER JOIN "user" ON "user".cip = beer_review_user_thumbsup.cip
+    WHERE id_beer_review = :idBeerReview
+
--- src/beerpressure/db/review/brewery_review.clj
+++ src/beerpressure/db/review/brewery_review.clj
@@ -1,16 +1,68 @@
 (ns beerpressure.db.review.brewery-review
   (:require [environ.core :refer [env]]
             [yesql.core :refer [defqueries]]
-            [beerpressure.db.common :refer [db-spec, check-error, convert-naming-convention]]))
+            [clojure.string :as str]
+            [beerpressure.db.common :refer :all]
+            [clojure.java.jdbc :as jdbc]))
 
 (defqueries "sql/review/operations_brewery_review.sql"
             {:connection db-spec})
 
+(defn fill-brewery-review-thumbsups
+  [brewery-review]
+  (assoc brewery-review :thumbsups
+                     (map #(hash-map :user %)
+                          (get-brewery-review-thumbsups {:idBreweryReview (get brewery-review :idBreweryReview)}))))
+
 (defn resolve-brewery-review
   [context args _value]
-  (first
-    (convert-naming-convention (check-error (get-brewery-review args)))))
+  (let [brewery-review (first
+                      (convert-naming-convention (check-error (get-brewery-review args))))]
+    (fill-brewery-review-thumbsups brewery-review)))
+
+(defn generate-brewery-reviews-query-beginning
+  [args]
+  "SELECT id_brewery_review, cip, id_brewery, title, content, image_path, rating, time FROM brewery_review")
+
+(defn generate-brewery-reviews-query-in-clause-breweries
+  [args]
+  (let [breweries (get args :breweries)]
+    (if (not= breweries [])
+      (str "id_brewery IN " (integer-list-to-in-clause breweries))
+      "")))
+
+(defn generate-brewery-reviews-query-in-clause-cips
+  [args]
+  (let [cips (get args :cips)]
+    (if (not= cips [])
+      (str "cip IN " (cip-list-to-in-clause cips))
+      "")))
+
+(defn generate-brewery-reviews-query-where
+  [args]
+  (let [in-clauses [(generate-brewery-reviews-query-in-clause-breweries args)
+                    (generate-brewery-reviews-query-in-clause-cips args)]
+        valid-in-clauses (filter #(not= "" %) in-clauses)]
+    (if (not= valid-in-clauses [])
+      (str "WHERE " (str/join " AND " valid-in-clauses))
+      "")))
+
+(defn generate-brewery-reviews-query-order-by
+  [args]
+  (case (get args :orderBy)
+    :TIME (case (get args :orderType)
+            :ASC "ORDER BY time ASC"
+            :DESC "ORDER BY time DESC")))
+
+(defn generate-brewery-reviews-query
+  [args]
+  (str (generate-brewery-reviews-query-beginning args) " "
+       (generate-brewery-reviews-query-where args) " "
+       (generate-brewery-reviews-query-order-by args) " "
+       (generate-query-limit-offset args)))
 
 (defn resolve-brewery-reviews
   [context args _value]
-  (convert-naming-convention (check-error (get-brewery-reviews args))))
+  (let [brewery-reviews (convert-naming-convention
+                       (check-error (jdbc/query db-spec (generate-brewery-reviews-query args))))]
+    (map #(fill-brewery-review-thumbsups %) brewery-reviews)))
