3f64c8a5336f6b5449883960db2a94ea8e26cff7
Marc-Antoine <maheux.marcantoine@gmail.com>
2018-06-20 10:13:22 -0400

    Add thumbsup count ordering (beerReviews and breweryReviews queries)

--- resources/sql/review/operations_brewery_review.sql
+++ resources/sql/review/operations_brewery_review.sql
@@ -1,5 +1,5 @@
 -- name: get-brewery-review
-SELECT id_brewery_review, cip, id_brewery, title, content, image_path, rating, time FROM Brewery_Review
+SELECT id_brewery_review, cip, id_brewery, title, content, image_path, rating, time FROM brewery_review
 WHERE id_brewery_review = :idBreweryReview;
 
 -- name: get-brewery-review-thumbsups
