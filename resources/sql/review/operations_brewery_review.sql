959b251471c2e19d855d193de9e7ed1f3924dd60
Marc-Antoine <maheux.marcantoine@gmail.com>
2018-06-19 22:31:38 -0400

    Add ordering, filtering and thumbsup to brewery review

--- resources/sql/review/operations_brewery_review.sql
+++ resources/sql/review/operations_brewery_review.sql
@@ -2,6 +2,7 @@
 SELECT id_brewery_review, cip, id_brewery, title, content, image_path, rating, time FROM Brewery_Review
 WHERE id_brewery_review = :idBreweryReview;
 
--- name: get-brewery-reviews
-SELECT id_brewery_review, cip, id_brewery, title, content, image_path, rating, time FROM Brewery_Review
-LIMIT :first OFFSET :skip;
+-- name: get-brewery-review-thumbsups
+SELECT "user".cip, name, surname FROM brewery_review_user_thumbsup
+    INNER JOIN "user" ON "user".cip = brewery_review_user_thumbsup.cip
+    WHERE id_brewery_review = :idBreweryReview
